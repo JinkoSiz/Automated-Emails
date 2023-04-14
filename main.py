6a7495472fa5931687be2e09310f8e415f8f06e9
JinkoSiz <zpska1977@mail.ru>
2023-04-14 19:10:04 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+import time
+
+import yagmail
+import pandas
+from news import NewsFeed
+import datetime
+
+while True:
+    if datetime.datetime.now().hour == 17 and datetime.datetime.now().minute == 43:
+        df = pandas.read_excel('people.xlsx')
+
+        for index, row in df.iterrows():
+            today = datetime.datetime.now().strftime('%Y-%m-%d')
+            yesterday = (datetime.datetime.now() - datetime.timedelta(days=1)).strftime('%Y-%m-%d')
+            news_feed = NewsFeed(interest=row['interest'],
+                                 from_date=yesterday,
+                                 to_date=today,
+                                 language='ru')
+            email = yagmail.SMTP(user='gmail', password='password')
+            email.send(to=row['email'],
+                       subject=f"Your {row['interest']} news for today!",
+                       contents=f"Hi {row['name']}\nSee what's on about {row['interest']} today. {news_feed.get()}")
+    time.sleep(60)
